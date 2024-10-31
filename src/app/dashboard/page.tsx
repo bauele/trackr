7d0e37f118ef682131e1d845332cd8bafb63d34d
bauele <bauele@users.noreply.github.com>
2024-10-31 13:18:11 -0400

    Improve dashboard styling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,34 +4,84 @@ import classNames from "classnames";
 export default function Login() {
   return (
     <div className={styles.page_container}>
-      <div className={styles.heading_container}>
-        <div className={styles.heading_content}>
-          <h1>Kyle's Inventory</h1>
-          <h2>Last Modified: Today</h2>
+      <div className={styles.content_container}>
+        <div className={styles.page_upper_container}>
+          <div className={styles.heading_container}>
+            <div className={styles.heading_content}>
+              <h1>Kyle's Inventory</h1>
+              <h2>Last Modified: Today</h2>
+            </div>
+            <div>
+              <button className={classNames("button", styles.button)}>
+                Log Out
+              </button>
+            </div>
+          </div>
+          <div className={styles.inventory_table_controls}>
+            <button className={classNames("button", styles.button)}>
+              Add Item
+            </button>
+            <div className={styles.sort_controls}>
+              <label htmlFor="sort">Sort By</label>
+              <select name="sort" id="sort">
+                <option value="date_added">Date Added</option>
+                <option value="item_name">Item Name</option>
+                <option value="quantity">Quantity</option>
+                <option value="last_modified">Last Modified</option>
+              </select>
+            </div>
+          </div>
         </div>
-        <div>
-          <button className={classNames("button", styles.button)}>
-            Log Out
-          </button>
-        </div>
+
+        <table className={styles.inventory_table}>
+          <tbody>
+            <tr>
+              <th>
+                <input type="checkbox" />
+              </th>
+
+              <th>Item Name</th>
+              <th>Quantity</th>
+            </tr>
+            <tr className={styles.item_record}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>
+                Blanket
+                <td>10/30/2024</td>
+              </td>
+              <td>
+                3<td>10/30/2024</td>
+              </td>
+            </tr>
+            <tr className={styles.item_record}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>
+                Blanket
+                <td>10/30/2024</td>
+              </td>
+              <td>
+                3<td>10/30/2024</td>
+              </td>
+            </tr>
+            <tr className={styles.item_record}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>
+                Blanket
+                <td>10/30/2024</td>
+              </td>
+              <td>
+                3<td>10/30/2024</td>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <button className={classNames("button", styles.button)}>Add Item</button>
-      <table className={styles.inventory_table}>
-        <tbody>
-          <tr>
-            <th>Date Added</th>
-            <th>Item Name</th>
-            <th>Quantity</th>
-            <th>Last Modified</th>
-          </tr>
-          <tr>
-            <td>10/30/2024</td>
-            <td>Blanket</td>
-            <td>3</td>
-            <td>10/30/2024</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   );
 }
